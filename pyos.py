43e634b6c927b69fcac9fe4390a9ea51ae8496df
AMDISYES <99589791+AMDISYES@users.noreply.github.com>
2024-08-25 09:57:58 +0800

    Add files via upload

--- pyos.py
+++ pyos.py
@@ -4,14 +4,14 @@ import datetime
 import calendar
 import os
 import sys
-import colorama
 import random
 import base64
+from dialog import Dialog
 from colorama import init, Fore, Back, Style
 init(autoreset = True)
 clsn = 0
 error = 0
-version = "2.0"
+version = "2.1"
 pkg = "8 (sys)"
 tips = ["You can find the default password in the passwd file.", "Maybe the coverter is useless :)", "'Root' is the default user.", "Is this file system real?", "Columns make the calculator work."]
 while clsn != 1:
@@ -228,14 +228,14 @@ while count < 3:
                         print(Fore.BLUE + "root" + Fore.RESET + "@" + Fore.BLUE + "localhost")
                         print("-----------------")
                         tm.sleep(0.05)
-                        print(Fore.BLUE + "OS" + Fore.RESET + ": MPGA PyOS V2.0 aarch64")
+                        print(Fore.BLUE + "OS" + Fore.RESET + ": MPGA PyOS V" + version + " aarch64")
                         if cls == "1":
                             host = "Windows CMD"
                         elif cls == "2":
                             host = "UNIX Shell"
                         tm.sleep(0.05)
                         print(Fore.BLUE + "Host" + Fore.RESET + ": " + host)
-                        print(Fore.BLUE + "Kernel" + Fore.RESET + ": PTCORE-V20240716-aarch64")
+                        print(Fore.BLUE + "Kernel" + Fore.RESET + ": PTCORE-V20240824-aarch64")
                         tm.sleep(0.05)
                         print(Fore.BLUE + "Uptime" + Fore.RESET + ": 9d, 4h, 19m, 27s")
                         tm.sleep(0.05)
@@ -259,11 +259,17 @@ while count < 3:
                             i = os.system("clear")
                     elif cmd == "shutdown":
                         error = 0
-                        print(Fore.BLUE + "Shutting down")
-                        for i in range(5):
-                            tm.sleep(0.5)
-                            print(".", end="")
-                        tm.sleep(0.15)
+                        d = Dialog(dialog = "dialog")
+                        d.set_background_title("MPGA PyOS V2.1 Build 20240824")
+                        d.infobox("Shutting down", width = 0, height = 0, title = "PyOS Power Manager")
+                        tm.sleep(0.5)
+                        d.infobox("Shutting down.", width = 0, height = 0, title = "PyOS Power Manager")
+                        tm.sleep(1)
+                        d.infobox("Shutting down..", width = 0, height = 0, title = "PyOS Power Manager")
+                        tm.sleep(1)
+                        d.infobox("Shutting down...", width = 0, height = 0, title = "PyOS Power Manager")
+                        tm.sleep(0.5)
+                        d.msgbox("You can now safely shut down PyOS.", width = 0, height = 0, title = "PyOS Power Manager")
                         if cls == "1":
                             i = os.system("cls")
                         elif cls == "2":
