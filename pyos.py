c342d3986a101ce120bbad2b91a0437a2007e3ba
Meltide <amdisyes@outlook.com>
2024-10-13 12:51:37 +0800

    Add files via upload

--- pyos.py
+++ pyos.py
@@ -160,19 +160,45 @@ while count < 3:
                         error = 0
                         ascount = 0
                         while ascount == 0:
-                            print("Enter the character you want to convert to ASCII")
+                            print(Fore.BLUE + "ASCII Dic")
+                            print("Choose the mode\n(1) Chr to ASCII\n(2) ASCII to Chr")
                             print(Style.DIM + "Press 'exit' to exit.")
-                            ascii = input("> ")
-                            length = len(ascii)
-                            if ascii == "exit":
+                            asciic = input("> ")
+                            if asciic == "1":
+                                while ascount == 0:
+                                    print("Enter the character you want to convert to ASCII")
+                                    print(Style.DIM + "Press 'exit' to exit.")
+                                    ascii = input("> ")
+                                    length = len(ascii)
+                                    if ascii == "exit":
+                                        break
+                                    elif ascii == "":
+                                        space = 0
+                                    else:
+                                        if length == 1:
+                                            print("Result: " + Fore.BLUE + str(ord(ascii)))
+                                        else:
+                                            print(Fore.RED + "Only a single character is supported.")
+                            elif asciic == "2":
+                                while ascount == 0:
+                                    print("Enter the ASCII code you want to convert to character")
+                                    print(Style.DIM + "Press 'exit' to exit.")
+                                    aschx = input("> ")
+                                    if aschx == "exit":
+                                        break
+                                    elif aschx == "":
+                                        space = 0
+                                    else:
+                                        try:
+                                            print("Result: " + Fore.BLUE + chr(int(aschx)))
+                                        except:
+                                            print(Fore.RED + "Invalid value.")
+                            elif asciic == "exit":
                                 break
-                            elif ascii == "":
+                            elif asciic == "":
                                 space = 0
                             else:
-                                if length == 1:
-                                    print("Result: " + Fore.BLUE + str(ord(ascii)))
-                                else:
-                                    print(Fore.RED + "Only a single character is supported.")
+                                print(Fore.RED + "Unknown command.")
                     elif cmd == "numgame":
                         error = 0
                         randnum = random.randint(100, 1000)
