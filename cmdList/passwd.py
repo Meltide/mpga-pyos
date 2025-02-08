638903032cd4d47ea9ab6834d5f615b6bfce6fa0
Adpro <101449500+adproqwq@users.noreply.github.com>
2025-02-09 02:39:51 +0800

    refactor: modularization

---
+++ cmdList/passwd.py
@@ -0,0 +1,14 @@
+from cmdList.registerCmd import registerCmd
+import json #解析和保存json配置文件
+import base64 #加解密库
+
+def passwd(self):
+    self.error = 0
+    npassword = input("Input new password: ")
+    with open("pwd", "r+") as pswd:
+        bs64 = str(base64.b64encode(npassword.encode("utf-8")))
+        pswd.truncate()
+        pswd.write(bs64.strip("b'"))
+    print("The password takes effect after the restart.")
+
+registerCmd().register("passwd", "Change your password", "System", passwd)
