44ec690c09b89bf59ad82799ccac0a66683e18ca
Meltide <amdisyes@outlook.com>
2025-04-06 10:14:30 +0800

    使用Colipot优化了代码

--- man.py
+++ man.py
@@ -1,34 +1,57 @@
-import importlib,json,os,subprocess
+import importlib
+import json
+import os
+import subprocess
+
 
 class CommandManager:
-    def __init__(self,core,cmd=""):
+    def __init__(self, core, cmd=""):
         self.cmd = cmd
         self.core = core
-        with open("config.json","r") as f:
-            self.cfg=json.load(f)
-            self.allcmds=self.cfg["commands"]
-        self.thirds=self.allcmds["Third-party"]
-        self.cmds=[cmd for category in self.allcmds.values() for cmd in category]
-    def reg(self,cmd):
-        self.cmd=cmd
+
+        # 加载配置文件
+        with open("config.json", "r") as f:
+            self.cfg = json.load(f)
+            self.allcmds = self.cfg["commands"]
+
+        self.thirds = self.allcmds["Third-party"]  # 第三方命令
+        self.cmds = [cmd for category in self.allcmds.values() for cmd in category]  # 所有命令列表
+
+    def reg(self, cmd):
+        """注册命令"""
+        self.cmd = cmd
+
     def pkg_name(self):
-        return "cmdList.third_party."+self.cmd if self.cmd in self.thirds else "cmdList."+self.cmd
-    def hasattr(self,attr):
-        return hasattr(self.getpkg(),attr)
+        """获取命令对应的包名"""
+        return "cmdList.third_party." + self.cmd if self.cmd in self.thirds else "cmdList." + self.cmd
+
+    def hasattr(self, attr):
+        """检查命令模块是否具有指定属性"""
+        return hasattr(self.getpkg(), attr)
+
     def loaded_cmd(self):
+        """检查命令是否已加载"""
         return self.cmd in self.cmds
+
     def getpkg(self):
+        """导入命令对应的模块"""
         return importlib.import_module(self.pkg_name())
-    def execute(self,args=()):
-        '''支持带参数的命令'''
+
+    def execute(self, args=()):
+        """执行命令，支持带参数"""
         if self.loaded_cmd():
-            __import__(self.pkg_name(),fromlist=["execute"]).execute(*args)
+            pkg_name = self.pkg_name()
+            __import__(pkg_name, fromlist=["execute"]).execute(*args)
         elif self.core.runsys:
-            subprocess.run([self.cmd]+list(args[1]))
+            try:
+                subprocess.run([self.cmd] + list(args[1]), check=True)
+            except subprocess.CalledProcessError as e:
+                print(f"System command failed: {e}")
         else:
-            raise ImportError
+            raise ImportError(f"Command '{self.cmd}' not found or not allowed to run system commands.")
+
 
 class PathManager:
-    def __init__(self,core):
-        self.core=core
-        self.path=os.getcwd()
+    def __init__(self, core):
+        self.core = core
+        self.path = os.getcwd()
--- pyosLogin.py
+++ pyosLogin.py
@@ -1,82 +1,123 @@
 from pyosInit import Init
-import base64 #加解密库
-from colorama import Fore, Back, Style #彩色文字库
-import time,datetime #时间日期库
-import json #解析和保存json配置文件
-import pwinput #密码隐藏库
-import random #随机库
+import base64  # 加解密库
+from colorama import Fore, Back, Style  # 彩色文字库
+import time, datetime  # 时间日期库
+import json  # 解析和保存json配置文件
+import pwinput  # 密码隐藏库
+
 
 class login(Init):
     def __init__(self):
         super().__init__()
         try:
-            times = datetime.datetime.now()
+            self.times = datetime.datetime.now()
             while self.count < 3:
                 self.user = input(f"{self.hostname} login: ")
-                if self.user=="create": #可新建账户
-                    newname=input('Name: ')
-                    newpwd=pwinput.pwinput()
-                    if newname in self.names:
-                        self.fprint("WARNING: The name was created!",2)
-                    elif newname in ("create","reset name","reset pwd"): #防止卡出bug
-                        self.fprint("Invalid username!",3)
-                    else:
-                        self.cfg["accounts"][newname]=base64.b64encode(newpwd.encode("utf-8")).decode("utf-8")
-                        with open("config.json","r+",encoding="utf-8") as f:
-                            json.dump(self.cfg,f,ensure_ascii=False,indent=4)
-                        self.fprint('Created successfully.',1)
-                elif self.user=="reset name": #重置用户名
-                    oldname=input('OldName: ')
-                    stpasswd = base64.b64decode(self.cfg["accounts"][oldname].strip()).decode("utf-8")
-                    pwd=pwinput.pwinput()
-                    if pwd==stpasswd and oldname in self.cfg["accounts"].keys() and oldname!="root":
-                        newname=input("NewName: ")
-                        del self.cfg["accounts"][oldname]
-                        self.cfg["accounts"][newname]=base64.b64encode(pwd.encode("utf-8")).decode("utf-8")
-                        with open("config.json","r+",encoding="utf-8") as f:
-                            json.dump(self.cfg,f,ensure_ascii=False,indent=4)
-                        self.fprint('Resetted successfully.',1)
-                    else:
-                        self.fprint("ERROR: Invalid username or password!",3)
-                elif self.user=="reset pwd": #重置密码
-                    name=input('Name: ')
-                    stpasswd = base64.b64decode(self.cfg["accounts"][name].strip()).decode("utf-8")
-                    oldpwd=pwinput.pwinput("OldPassword: ")
-                    if oldpwd==stpasswd and oldname in self.cfg["accounts"].keys() and oldname!="root":
-                        newpwd=pwinput.pwinput("NewPassword: ")
-                        self.cfg["accounts"][name]=base64.b64encode(newpwd.encode("utf-8")).decode("utf-8")
-                        with open("config.json","r+",encoding="utf-8") as f:
-                            json.dump(self.cfg,f,ensure_ascii=False,indent=4)
-                        print(f'{Fore.GREEN}Resetted successfully.')
-                    else:
-                        print(f"{Fore.RED}ERROR: Invalid username or password!")
-                elif self.user in self.names: #正常登录
-                    stpasswd = base64.b64decode(self.cfg["accounts"][self.user].strip()).decode("utf-8")
-                    while self.count < 3:
-                        passwd = pwinput.pwinput()
-                        if passwd == stpasswd:
-                            print("Last login: "+ Fore.CYAN+ times.strftime("%y/%m/%d %H:%M:%S"))
-                            time.sleep(0.45)
-                            print("")
-                            if self.runsys:
-                                self.fprint("WARNING: Running system commands is enabled!",2)
-                            while self.count < 3:
-                                zshp9k_tm = datetime.datetime.now()
-                                zshp9k_pre = zshp9k_tm.strftime(" %m/%d %H:%M:%S ")
-                                zshp9k = zshp9k_pre
-                                if self.errcode:
-                                    self.cmd = input(f"{Back.RED}{Fore.WHITE} ✘ {self.errcode} {Back.WHITE}{Fore.BLACK}{zshp9k}{Back.YELLOW} {self.user}@{self.hostname} {Back.BLUE}{Fore.WHITE} {self.file} {Back.RESET}> ")
-                                else:
-                                    self.cmd = input(f"{Back.WHITE}{Fore.BLACK}{zshp9k}{Back.YELLOW} {self.user}@{self.hostname} {Back.BLUE}{Fore.WHITE} {self.file} {Back.RESET}> ")
-                                self.run(self.cmd)
-                        elif passwd == "":
-                            print(f"{Style.DIM}Tip: You can find the default password in the passwd file.")
-                        else:
-                            print("Error password! Please retry")
-                            print(f"{Style.DIM}Tip: You can find the default password in the passwd file.")
+                if self.user == "create":
+                    self.create_account()
+                elif self.user == "reset name":
+                    self.reset_username()
+                elif self.user == "reset pwd":
+                    self.reset_password()
+                elif self.user in self.names:
+                    self.login_user()
                 else:
-                    print("Invalid user! Please retry")
+                    self.fprint("Invalid user! Please retry", 3)
                     print(Style.DIM + "Tip: 'root' is the default user.")
-        except (KeyboardInterrupt,EOFError):
-            self.fprint("\nYou exited PyOS just now!",3)
+        except (KeyboardInterrupt, EOFError):
+            self.fprint("\nYou exited PyOS just now!", 3)
             quit(114)
+
+    def save_config(self):
+        """保存配置到文件"""
+        with open("config.json", "r+", encoding="utf-8") as f:
+            json.dump(self.cfg, f, ensure_ascii=False, indent=4)
+
+    def encode_password(self, password):
+        """加密密码"""
+        return base64.b64encode(password.encode("utf-8")).decode("utf-8")
+
+    def decode_password(self, encoded_password):
+        """解密密码"""
+        return base64.b64decode(encoded_password.strip()).decode("utf-8")
+
+    def create_account(self):
+        """创建新账户"""
+        newname = input("Name: ")
+        newpwd = pwinput.pwinput()
+        if newname in self.names:
+            self.fprint("WARNING: The name was created!", 2)
+        elif newname in ("create", "reset name", "reset pwd"):
+            self.fprint("Invalid username!", 3)
+        else:
+            self.cfg["accounts"][newname] = self.encode_password(newpwd)
+            self.save_config()
+            self.fprint("Created successfully.", 1)
+
+    def reset_username(self):
+        """重置用户名"""
+        oldname = input("OldName: ")
+        if oldname in self.cfg["accounts"].keys() and oldname != "root":
+            stpasswd = self.decode_password(self.cfg["accounts"][oldname])
+            pwd = pwinput.pwinput()
+            if pwd == stpasswd:
+                newname = input("NewName: ")
+                del self.cfg["accounts"][oldname]
+                self.cfg["accounts"][newname] = self.encode_password(pwd)
+                self.save_config()
+                self.fprint("Resetted successfully.", 1)
+            else:
+                self.fprint("ERROR: Invalid username or password!", 3)
+        else:
+            self.fprint("ERROR: Invalid username or password!", 3)
+
+    def reset_password(self):
+        """重置密码"""
+        name = input("Name: ")
+        if name in self.cfg["accounts"].keys() and name != "root":
+            stpasswd = self.decode_password(self.cfg["accounts"][name])
+            oldpwd = pwinput.pwinput("OldPassword: ")
+            if oldpwd == stpasswd:
+                newpwd = pwinput.pwinput("NewPassword: ")
+                self.cfg["accounts"][name] = self.encode_password(newpwd)
+                self.save_config()
+                self.fprint("Resetted successfully.", 1)
+            else:
+                self.fprint("ERROR: Invalid username or password!", 3)
+        else:
+            self.fprint("ERROR: Invalid username or password!", 3)
+
+    def login_user(self):
+        """用户登录"""
+        stpasswd = self.decode_password(self.cfg["accounts"][self.user])
+        while self.count < 3:
+            passwd = pwinput.pwinput()
+            if passwd == stpasswd:
+                print("Last login: " + Fore.CYAN + self.times.strftime("%y/%m/%d %H:%M:%S"))
+                time.sleep(0.45)
+                print("")
+                if self.runsys:
+                    self.fprint("WARNING: Running system commands is enabled!", 2)
+                self.start_shell()
+                break
+            elif passwd == "":
+                print(f"{Style.DIM}Tip: You can find the default password in the passwd file.")
+            else:
+                self.fprint("Error password! Please retry", 3)
+                print(f"{Style.DIM}Tip: You can find the default password in the passwd file.")
+
+    def start_shell(self):
+        """启动命令行交互"""
+        while self.count < 3:
+            zshp9k_tm = datetime.datetime.now()
+            zshp9k_pre = zshp9k_tm.strftime(" %m/%d %H:%M:%S ")
+            zshp9k = zshp9k_pre
+            if self.errcode:
+                self.cmd = input(
+                    f"{Back.RED}{Fore.WHITE} ✘ {self.errcode} {Back.WHITE}{Fore.BLACK}{zshp9k}{Back.YELLOW} {self.user}@{self.hostname} {Back.BLUE}{Fore.WHITE} {self.file} {Back.RESET}> "
+                )
+            else:
+                self.cmd = input(
+                    f"{Back.WHITE}{Fore.BLACK}{zshp9k}{Back.YELLOW} {self.user}@{self.hostname} {Back.BLUE}{Fore.WHITE} {self.file} {Back.RESET}> "
+                )
+            self.run(self.cmd)
