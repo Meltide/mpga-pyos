5a2f936051b6e421f32f917b52bfd8a472ec7cfd
Meltide <amdisyes@outlook.com>
2025-05-01 14:37:17 +0800

    优化代码，添加自动安装依赖

--- pyosInit.py
+++ pyosInit.py
@@ -12,37 +12,57 @@ from man import CommandManager
 class Init:  # 初始化
     def __init__(self):
         init(autoreset=True)
-        self.cmdman = CommandManager(self, self)
-        self.clsn = 0
-        self.errcode = 0
-        self.tips = [
+        self.command_manager = CommandManager(self, self)  # 命令管理器
+        self.clear_screen_count = 0  # 清屏计数
+        self.error_code = 0  # 错误代码
+        self.tips_list = [  # 提示列表
             "You can find the default password in the passwd file.",
             "Maybe the coverter is useless :)",
             "'root' is the default user.",
             "Is this file system real?",
             "Columns make the calculator work."
         ]
-        self.selected_tip = random.choice(self.tips)  # 提前选择随机提示
+        self.selected_tip = random.choice(self.tips_list)  # 随机选择提示
         self.color_modes = [Fore.WHITE, Fore.GREEN, Fore.YELLOW, Fore.RED]  # 颜色模式列表
 
-        # 读取配置
-        with open("config.json", "r", encoding="utf-8") as f:
-            self.cfg = json.load(f)
-            self.names = self.cfg["accounts"].keys()
-            self.hostname = self.cfg["hostname"]
-            self.runsys = self.cfg["os.system"]  # 是否支持运行系统命令
-            self.ver = "2.8"
-            self.pyshver = "1.2.0"
-            self.core = "20250405"
+        # 加载配置
+        self._load_config()
 
+        # 显示启动进度条
         time.sleep(0.5)
         clear(self, [])
         self._show_progress_bar()
         clear(self, [])
 
         # 打印启动信息
-        self.printlist = [
-            Style.DIM + f"\nMPGA PyOS Open Source System {self.ver}",
+        self._print_startup_messages()
+
+        self.command_count = 0  # 命令计数
+        self.current_directory = "~"  # 当前目录
+
+    def _load_config(self):
+        """加载配置文件"""
+        with open("config.json", "r", encoding="utf-8") as config_file:
+            self.config = json.load(config_file)
+            self.account_names = self.config["accounts"].keys()  # 账户名称
+            self.hostname = self.config["hostname"]  # 主机名
+            self.allow_system_commands = self.config["os.system"]  # 是否支持运行系统命令
+            self.version = "2.9"  # 系统版本
+            self.shell_version = "1.2.0"  # Shell 版本
+            self.core_version = "20250405"  # 核心版本
+
+    def _show_progress_bar(self):
+        """显示启动进度条"""
+        for progress in range(1, 101):
+            sys.stdout.write(f"\rStarting: {progress}%: {'=' * (progress // 8)}")
+            sys.stdout.flush()
+            time.sleep(0.005)
+        print()  # 换行
+
+    def _print_startup_messages(self):
+        """打印启动信息"""
+        startup_messages = [
+            Style.DIM + f"\nMPGA PyOS Open Source System {self.version}",
             Fore.BLUE + text2art("MPGA", font="small"),
             Fore.YELLOW + "Make PyOS Great Again!\n",
             f"Tip: {self.selected_tip}",
@@ -54,25 +74,14 @@ class Init:  # 初始化
             Fore.CYAN
             + "\nAlso try PyOS's improved version by minqwq and bibimingming!\n",
         ]
-        for line in self.printlist:
-            print(line)
+        for message in startup_messages:
+            print(message)
             time.sleep(0.1)
 
-        self.count = 0
-        self.file = "~"
-
-    def _show_progress_bar(self):
-        """显示启动进度条"""
-        for i in range(1, 101):
-            sys.stdout.write(f"\rStarting: {i}%: {'=' * (i // 8)}")
-            sys.stdout.flush()
-            time.sleep(0.005)
-        print()  # 换行
-
-    def fprint(self, msg, mode=0):
+    def fprint(self, message, mode=0):
         """打印消息
         mode: 0白色，1绿色，2黄色，3红色
         """
-        print(self.color_modes[mode] + msg)
+        print(self.color_modes[mode] + message)
         if mode == 3:
-            self.errcode = random.randint(100, 999)
+            self.error_code = random.randint(100, 999)
--- pyosLogin.py
+++ pyosLogin.py
@@ -1,3 +1,4 @@
+import sys  # 系统库
 from pyosInit import Init
 import base64  # 加解密库
 from colorama import Fore, Back, Style  # 彩色文字库
@@ -6,32 +7,32 @@ import json  # 解析和保存json配置文件
 import pwinput  # 密码隐藏库
 
 
-class login(Init):
+class Login(Init):
     def __init__(self):
         super().__init__()
         try:
-            self.times = datetime.datetime.now()
-            while self.count < 3:
-                self.user = input(f"{self.hostname} login: ")
-                if self.user == "create":
+            self.current_time = datetime.datetime.now()
+            self.max_attempts = 3
+            while self.command_count < self.max_attempts:
+                self.username = input(f"{self.hostname} login: ")
+                if self.username == "create":
                     self.create_account()
-                elif self.user == "reset name":
+                elif self.username == "reset name":
                     self.reset_username()
-                elif self.user == "reset pwd":
+                elif self.username == "reset pwd":
                     self.reset_password()
-                elif self.user in self.names:
+                elif self.username in self.account_names:
                     self.login_user()
                 else:
-                    self.fprint("Invalid user! Please retry", 3)
-                    print(Style.DIM + "Tip: 'root' is the default user.")
+                    self._invalid_user_message()
         except (KeyboardInterrupt, EOFError):
             self.fprint("\nYou exited PyOS just now!", 3)
-            quit(114)
+            sys.exit(114)  # 使用 sys.exit() 优雅退出程序
 
     def save_config(self):
         """保存配置到文件"""
         with open("config.json", "r+", encoding="utf-8") as f:
-            json.dump(self.cfg, f, ensure_ascii=False, indent=4)
+            json.dump(self.config, f, ensure_ascii=False, indent=4)
 
     def encode_password(self, password):
         """加密密码"""
@@ -43,81 +44,93 @@ class login(Init):
 
     def create_account(self):
         """创建新账户"""
-        newname = input("Name: ")
-        newpwd = pwinput.pwinput()
-        if newname in self.names:
-            self.fprint("WARNING: The name was created!", 2)
-        elif newname in ("create", "reset name", "reset pwd"):
+        new_username = input("Name: ")
+        new_password = pwinput.pwinput()
+        if new_username in self.account_names:
+            self.fprint("WARNING: The name already exists!", 2)
+        elif new_username in ("create", "reset name", "reset pwd"):
             self.fprint("Invalid username!", 3)
         else:
-            self.cfg["accounts"][newname] = self.encode_password(newpwd)
+            self.config["accounts"][new_username] = self.encode_password(new_password)
             self.save_config()
-            self.fprint("Created successfully.", 1)
+            self.fprint("Account created successfully.", 1)
 
     def reset_username(self):
         """重置用户名"""
-        oldname = input("OldName: ")
-        if oldname in self.cfg["accounts"].keys() and oldname != "root":
-            stpasswd = self.decode_password(self.cfg["accounts"][oldname])
-            pwd = pwinput.pwinput()
-            if pwd == stpasswd:
-                newname = input("NewName: ")
-                del self.cfg["accounts"][oldname]
-                self.cfg["accounts"][newname] = self.encode_password(pwd)
-                self.save_config()
-                self.fprint("Resetted successfully.", 1)
-            else:
-                self.fprint("ERROR: Invalid username or password!", 3)
+        old_username = input("OldName: ")
+        if self._validate_user(old_username):
+            new_username = input("NewName: ")
+            self.config["accounts"][new_username] = self.config["accounts"].pop(old_username)
+            self.save_config()
+            self.fprint("Username reset successfully.", 1)
         else:
             self.fprint("ERROR: Invalid username or password!", 3)
 
     def reset_password(self):
         """重置密码"""
-        name = input("Name: ")
-        if name in self.cfg["accounts"].keys() and name != "root":
-            stpasswd = self.decode_password(self.cfg["accounts"][name])
-            oldpwd = pwinput.pwinput("OldPassword: ")
-            if oldpwd == stpasswd:
-                newpwd = pwinput.pwinput("NewPassword: ")
-                self.cfg["accounts"][name] = self.encode_password(newpwd)
-                self.save_config()
-                self.fprint("Resetted successfully.", 1)
-            else:
-                self.fprint("ERROR: Invalid username or password!", 3)
+        username = input("Name: ")
+        if self._validate_user(username):
+            new_password = pwinput.pwinput("NewPassword: ")
+            self.config["accounts"][username] = self.encode_password(new_password)
+            self.save_config()
+            self.fprint("Password reset successfully.", 1)
         else:
             self.fprint("ERROR: Invalid username or password!", 3)
 
     def login_user(self):
         """用户登录"""
-        stpasswd = self.decode_password(self.cfg["accounts"][self.user])
-        while self.count < 3:
-            passwd = pwinput.pwinput()
-            if passwd == stpasswd:
-                print("Last login: " + Fore.CYAN + self.times.strftime("%y/%m/%d %H:%M:%S"))
-                time.sleep(0.45)
-                print("")
-                if self.runsys:
-                    self.fprint("WARNING: Running system commands is enabled!", 2)
+        stored_password = self.decode_password(self.config["accounts"][self.username])
+        while self.command_count < self.max_attempts:
+            entered_password = pwinput.pwinput()
+            if entered_password == stored_password:
+                self._successful_login_message()
                 self.start_shell()
                 break
-            elif passwd == "":
-                print(f"{Style.DIM}Tip: You can find the default password in the passwd file.")
             else:
-                self.fprint("Error password! Please retry", 3)
-                print(f"{Style.DIM}Tip: You can find the default password in the passwd file.")
+                self._invalid_password_message()
 
     def start_shell(self):
         """启动命令行交互"""
-        while self.count < 3:
-            zshp9k_tm = datetime.datetime.now()
-            zshp9k_pre = zshp9k_tm.strftime(" %m/%d %H:%M:%S ")
-            zshp9k = zshp9k_pre
-            if self.errcode:
-                self.cmd = input(
-                    f"{Back.RED}{Fore.WHITE} ✘ {self.errcode} {Back.WHITE}{Fore.BLACK}{zshp9k}{Back.YELLOW} {self.user}@{self.hostname} {Back.BLUE}{Fore.WHITE} {self.file} {Back.RESET}> "
-                )
-            else:
-                self.cmd = input(
-                    f"{Back.WHITE}{Fore.BLACK}{zshp9k}{Back.YELLOW} {self.user}@{self.hostname} {Back.BLUE}{Fore.WHITE} {self.file} {Back.RESET}> "
-                )
-            self.run(self.cmd)
+        while self.command_count < self.max_attempts:
+            timestamp = datetime.datetime.now().strftime(" %m/%d %H:%M:%S ")
+            prompt = self._generate_prompt(timestamp)
+            command = input(prompt)
+            self.run(command)
+
+    def _validate_user(self, username):
+        """验证用户名和密码"""
+        if username in self.config["accounts"] and username != "root":
+            stored_password = self.decode_password(self.config["accounts"][username])
+            entered_password = pwinput.pwinput("Password: ")
+            return entered_password == stored_password
+        return False
+
+    def _invalid_user_message(self):
+        """打印无效用户提示"""
+        self.fprint("Invalid user! Please retry", 3)
+        print(Style.DIM + "Tip: 'root' is the default user.")
+
+    def _invalid_password_message(self):
+        """打印无效密码提示"""
+        self.fprint("Error password! Please retry", 3)
+        print(f"{Style.DIM}Tip: You can find the default password in the passwd file.")
+
+    def _successful_login_message(self):
+        """打印成功登录提示"""
+        print("Last login: " + Fore.CYAN + self.current_time.strftime("%y/%m/%d %H:%M:%S"))
+        time.sleep(0.45)
+        print("")
+        if self.allow_system_commands:
+            self.fprint("WARNING: Running system commands is enabled!", 2)
+
+    def _generate_prompt(self, timestamp):
+        """生成命令行提示符"""
+        if self.error_code:
+            return (
+                f"{Back.RED}{Fore.WHITE} ✘ {self.error_code} {Back.WHITE}{Fore.BLACK}{timestamp}"
+                f"{Back.YELLOW} {self.username}@{self.hostname} {Back.BLUE}{Fore.WHITE} {self.current_directory} {Back.RESET}> "
+            )
+        return (
+            f"{Back.WHITE}{Fore.BLACK}{timestamp}{Back.YELLOW} {self.username}@{self.hostname} "
+            f"{Back.BLUE}{Fore.WHITE} {self.current_directory} {Back.RESET}> "
+        )
