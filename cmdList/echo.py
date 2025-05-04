b7e4912dd60a4b0e9688970230f4106309a270e4
Meltide <amdisyes@outlook.com>
2025-05-04 16:30:35 +0800

    优化ls显示效果，文件夹靠前并标蓝，按名称排序

--- cmdList/echo.py
+++ cmdList/echo.py
@@ -1,6 +1,4 @@
 __doc__ = "Parameter return"
 
 def execute(self, args):
-    for message in args:
-        print(message, end=" ")
-    print("\b")
+    print(*args)
--- cmdList/ls.py
+++ cmdList/ls.py
@@ -1,6 +1,22 @@
-import os #系统底层库
+import os  # 系统底层库
+from colorama import Fore, Style  # 颜色库
 
-__doc__="View the path"
+__doc__ = "View the path"
 
-def execute(self,args):
-    print(*os.listdir(os.getcwd()))
+def execute(self, args):
+    items = os.listdir(os.getcwd())
+    
+    # 分离文件夹和文件，并按名称排序（不区分大小写）
+    folders = sorted(
+        [item for item in items if os.path.isdir(item)],
+        key=lambda x: x.lower()  # 按文件名排序（忽略大小写）
+    )
+    files = sorted(
+        [item for item in items if not os.path.isdir(item)],
+        key=lambda x: x.lower()  # 按文件名排序（忽略大小写）
+    )
+    
+    print(Fore.BLUE + "/\n".join(folders) + "/")
+    print("\n".join(files), end="")
+    
+    print()
