83a158b6410991e5e3b55ee361edfb8be1e5fe4d
EricDing618 <1843794003@qq.com>
2025-05-05 17:22:58 +0800

    fix

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,14 +1,9 @@
-from typing import Any
+import sys,os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utils.config import *
 
 class Test:
-    def __init__(self):
-        self.test_errcode()
-
     def test_errcode(self):
         '''查重测试'''
         assert len(set(EXCEPTION_RETURNS.values())) == len(EXCEPTION_RETURNS)
         assert len(set(EXCEPTION_INFO.keys())) == len(EXCEPTION_INFO)
-
-if __name__ == '__main__':
-    Test()
