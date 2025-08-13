304fe02a2ad18e1444f59808e9bfa26122c517b9
Eric学编程 <1843794003@qq.com>
2025-08-13 21:11:18 +0800

    Update Lexer.py

--- PYOScript/Lexer.py
+++ PYOScript/Lexer.py
@@ -1,8 +1,12 @@
 from collections import deque
 
 class TokenType:
-    def __init__(self, value: str):
+    def __init__(self, value: str, description: str = '', **kwargs):
         self.value = value
+        self.description = description
+        self.highlight = 'default'
+        for key, val in kwargs.items():
+            setattr(self, key, val)
 
     def __repr__(self):
         return f'TokenType({self.value!r})'
@@ -11,13 +15,21 @@ class TokenType:
         return self.value
 
     def __eq__(self, other):
-        if isinstance(other, String):
+        if isinstance(other, self.__class__):
             return self.value == other.value
         return False
 
     def __hash__(self):
         return hash(self.value)
 
+class Strip(TokenType):
+    def __repr__(self):
+        return f'Strip({self.value!r})'
+    
+class Info(TokenType):
+    def __repr__(self):
+        return f'Info({self.value!r})'
+    
 class String(TokenType):
     def __repr__(self):
         return f'String({self.value!r})'
@@ -35,14 +47,34 @@ class Lexer:
         self.code = code
         self.tokens = deque()
         self.stack = deque()
+        self.area = None
 
     def tokenize(self):
-        for line in self.code.splitlines():
-            if line.startswith('#'):
+        lines = self.code.splitlines()
+        keepend_lines = self.code.splitlines(keepends=True)
+        for i in range(len(keepend_lines)):
+            line = lines[i]
+            '''if line.startswith('#'):
                 continue  # Skip comments
-            if line.isdigit():
-                yield Number(line)
-            elif line.startswith('"') and line.endswith('"'):
-                yield String(line[1:-1])
-            else:
-                yield Others(line)
+            elif line.startswith('@'):
+
+            for col in line:
+                
+                if line.isdigit():
+                    yield Number(line)
+                elif line.startswith('"') and line.endswith('"'):
+                    yield String(line[1:-1])
+                else:
+                    yield Others(line)'''
+            for col in line:
+                if col in ('"', "'"):
+                    if self.area == String:
+                        self.area = None
+                        self.tokens.append(self.stack.pop())
+                    else:
+                        self.area = String
+                elif self.area == String:
+                    self.stack.append(col)
+                elif i > len(lines) - 1:
+                    self.tokens.append(Strip(col, 'n'))
+                    
